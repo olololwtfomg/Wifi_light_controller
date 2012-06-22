8c3346439ddd507a8f3031445bb4e93edab7a01f
olololwtfomg <olololwtfomg@gmail.com>
2012-06-22 20:34:02 +0200

    Working USART

--- FIRMWARE/main.c
+++ FIRMWARE/main.c
@@ -1,13 +1,24 @@
+
 /*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <olololwtfomg@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Jaroslav Mucka
+ * ----------------------------------------------------------------------------
  */
 
 #include <avr/io.h>
+#include "initialization.h"
+#include "USART.h"
 
 int main(void)
 {
+    //initializations
 
-    // Insert code
-
+    init_periph();
+    init_all();
+    debug_send("Welcome to hell!!!\r\n\0");
     while(1)
     ;
 
